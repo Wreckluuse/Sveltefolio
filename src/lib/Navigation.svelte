3d3151fca26955792cf879341506c707e31b7d69
Andrew Weightman <andrewjw797@gmail.com>
2023-02-22 18:50:57 -0800

    Initial commit

---
+++ src/lib/Navigation.svelte
@@ -0,0 +1,42 @@
+<script>
+</script>
+
+<div id="navContainer">
+	<div class="navItem">Contact</div>
+	<div class="navItem">About</div>
+	<div class="navItem">Colours</div>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+
+	#navContainer {
+		border-top: 4.5vw solid rgb(143, 166, 198);
+		border-left: 2vw solid transparent;
+		border-right: 2vw solid transparent;
+		height: 0;
+		width: 25vw;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: -2%;
+        display:grid;
+        grid-template-columns: 20% 20% 20%;
+        grid-gap: 20%;
+	}
+    .navItem {
+		font-family: 'Inter', sans-serif;
+        color: #232324;
+        text-align: center;
+		height: fit-content;
+		width: 90%;
+		margin-top: -38%;
+		margin-left: auto;
+		margin-right: auto;
+		font-size: 1em;
+    }
+
+	.navItem:hover{
+		color: white;
+	}
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Navigation from '$lib/Navigation.svelte';
+</script>
+
+<svelte:head>
+	<title>Portfolio</title>
+</svelte:head>
+<body>
+	<Navigation />
+</body>
+
+<style>
+	body {
+		position: fixed;
+		top: 0;
+		left: 0;
+		margin: 0;
+		height: 100vh;
+		width: 100vw;
+		background: hsla(212, 45%, 45%, 1);
+
+		background: linear-gradient(
+			90deg,
+			hsla(212, 45%, 45%, 1) 0%,
+			hsla(212, 51%, 42%, 1) 26%,
+			hsla(0, 0%, 100%, 1) 100%
+		);
+
+		background: -moz-linear-gradient(
+			90deg,
+			hsla(212, 45%, 45%, 1) 0%,
+			hsla(212, 51%, 42%, 1) 26%,
+			hsla(0, 0%, 100%, 1) 100%
+		);
+
+		background: -webkit-linear-gradient(
+			90deg,
+			hsla(212, 45%, 45%, 1) 0%,
+			hsla(212, 51%, 42%, 1) 26%,
+			hsla(0, 0%, 100%, 1) 100%
+		);
+	}
+</style>
